e4d3fbaa62b3fa1e85221e37da56331b0a8aa305
Kartik <kartikmehra173@gmail.com>
2025-04-07 17:07:50 +0530

    bug

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:19006',
+        origin: '*',
         methods: ['GET', 'POST']
     }
 })
