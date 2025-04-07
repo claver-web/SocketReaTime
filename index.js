b4751cf650d9efb4788cc6e2ff33f68cc299f7b9
Kartik <kartikmehra173@gmail.com>
2025-04-07 16:47:52 +0530

    Socket Message

---
+++ index.js
@@ -0,0 +1,43 @@
+import http from 'http';
+import express from 'express';
+import { Server } from 'socket.io';
+import cors from 'cors';
+
+import bodyParser from 'body-parser';
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:19006',
+        methods: ['GET', 'POST']
+    }
+})
+
+io.on('connection', (socket) => {
+    console.log(`Client connected: ${socket.id}`);
+
+    socket.on('message', (data) => {
+        console.log(`Message from ${socket.id}:`, data);
+
+        io.emit('message', {
+            id: socket.id,
+            text: data,
+            timeStamp: Date.now()
+        })
+    })
+
+    socket.on("disconnect", () => {
+        console.log(`Client disconnected: ${socket.id}`);
+    })
+})
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Socket.IO server running at http://localhost:${PORT}`);
+});
